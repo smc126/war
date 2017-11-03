4aaa01b6f6141c291158a82286112c5e0a62e89f
smc126 <smc126@WIU516970.ad.wiu.edu>
2017-11-03 12:01:55 -0500

    hi

---
+++ src/wargame/WarGame.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wargame;
+
+/**
+ *
+ * @author smc126
+ */
+public class WarGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InitialGui g = new InitialGui();
+        g.setVisible(true);
+    }
+    
+}
